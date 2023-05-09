702ff0de9d3e5da6d39bfc794ae16d1030d402f2
Simone Gentili <sensorario@gmail.com>
2023-05-09 16:56:59 +0200

    esercizi || uscire dal while con un flag o con break?

--- spesa/index.php
+++ spesa/index.php
@@ -8,16 +8,15 @@ $prodotti[] = [ 'nome' => 'banana', 'value' => 55 ];
 $prodotti[] = [ 'nome' => 'cocomero', 'value' => 66 ];
 $prodotti[] = [ 'nome' => 'datteri', 'value' => 333 ];
 $index = 0;
-$esci = false;
 
-while ($esci === false && $budget >= $spesa) {
+while ($budget >= $spesa) {
     $prodotto = $prodotti[$index++];
     if (($spesa + $prodotto['value']) > $budget) {
-        $esci = true;
-    } else {
-        $spesa += $prodotto['value'];
-        printf("\nPierino ha messo %s nel carrello.", $prodotto['nome']);
+        break;
     }
+    
+    $spesa += $prodotto['value'];
+    printf("\nPierino ha messo %s nel carrello.", $prodotto['nome']);
 }
 
 printf("\nPierino ha speso in totale %s.", $spesa);
