d5cdbf9735d77bc3bc2087e90d6fb8d41154b0f5
Simone Gentili <sensorario@gmail.com>
2023-05-10 02:37:27 +0200

    esercizi || completo la spesa

--- spesa/index.php
+++ spesa/index.php
@@ -3,20 +3,27 @@
 $budget = 400;
 $spesa = 0;
 $prodotti = [];
-$prodotti[] = [ 'nome' => 'arancia', 'value' => 42 ];
-$prodotti[] = [ 'nome' => 'banana', 'value' => 55 ];
-$prodotti[] = [ 'nome' => 'cocomero', 'value' => 66 ];
-$prodotti[] = [ 'nome' => 'datteri', 'value' => 333 ];
+$prodotti[] = [ 'nome' => 'arancia', 'value' => 3 ];
+$prodotti[] = [ 'nome' => 'banana', 'value' => 4 ];
+$prodotti[] = [ 'nome' => 'cocomero', 'value' => 4 ];
+$prodotti[] = [ 'nome' => 'datteri', 'value' => 666 ];
 $index = 0;
 
 while ($budget >= $spesa) {
-    $prodotto = $prodotti[$index++];
-    if (($spesa + $prodotto['value']) > $budget) {
-        break;
+    $current = $index++;
+    $prodotto = $prodotti[$current];
+    $skip = false;
+
+    if ($prodotto === null || ($spesa + $prodotto['value']) > $budget) {
+        $skip = true;
     }
-    
-    $spesa += $prodotto['value'];
-    printf("\nPierino ha messo %s nel carrello.", $prodotto['nome']);
+
+    if ($skip === false) {
+        $spesa += $prodotto['value'];
+        printf("\nPierino ha messo %s nel carrello.", $prodotto['nome']);
+    }
+
+    if (!isset($prodotti[$current + 1])) break;
 }
 
 printf("\nPierino ha speso in totale %s.", $spesa);
